294d0649db6f28776b1945c3a9f4b55fec577aa6
oguzkorkut <oguzkrkt@gmail.com>
2022-01-18 21:43:16 +0300

    Proje java based olarak değiştirildi

---
+++ android/app/src/main/java/com/okorkut/barber/booking/flutter_barber_booking/MainActivity.java
@@ -0,0 +1,6 @@
+package com.okorkut.barber.booking.flutter_barber_booking;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
--- android/app/src/main/kotlin/com/okorkut/barber/booking/flutter_barber_booking/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.okorkut.barber.booking.flutter_barber_booking
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
